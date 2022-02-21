872af32daf99c56df118d1b266c7bfc08ddb9fe0
amang <904698410@qq.com>
2022-02-21 19:29:23 +0800

    3.1 limits

---
+++ 3.1 limits/limits.cpp
@@ -0,0 +1,27 @@
+// limits.cpp -- some integer limits
+#include <iostream>
+#include <climits> //use limits.h for older systems
+int main() {
+	using namespace std;
+	int n_int = INT_MAX; // initialize n_int to max int value
+	short n_short = SHRT_MAX; // symbols defined in climits file
+	long n_long = LONG_MAX;
+	long long n_llong = LLONG_MAX;
+
+	//sizeof operator yields size of type or of variable
+	cout << "int is " << sizeof(int) << " bytes." << endl;
+	cout << "short is " << sizeof(short) << " bytes." << endl;
+	cout << "long is " << sizeof(long) << " bytes." << endl;
+	cout << "long long is " << sizeof(long long) << " bytes." << endl;
+	cout << endl;
+
+	cout << "Maximun values:" << endl;
+	cout << "int: " << n_int << endl;
+	cout << "short: " << n_short << endl;
+	cout << "long: " << n_long << endl;
+	cout << "long long: " << n_llong << endl;
+
+	cout << "Minimun int value = " << INT_MIN << endl;
+	cout << "Bits per byte = " << CHAR_BIT << endl;
+	return 0;
+}
