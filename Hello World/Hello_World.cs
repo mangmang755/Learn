7d77c4bb75506806525abcdbb409fa1c732f5a64
amang <904698410@qq.com>
2022-03-23 19:17:42 +0800

    Hello World

---
+++ Hello World/Hello_World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Hello_World
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
